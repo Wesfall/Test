94b223e1bcc86a5a422d2542eca16c37e6d711b7
Wesfall <krixaliks@gmail.com>
2018-10-02 00:46:23 +0300

    Fifth commit, changes with help from comments

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
-//Работу выполнил Шапошников Александр, группа 3110
 public class Main {
     private final static int ROWS = 6, COLUMNS = 11;        //константы - 6 строк, 11 столбцов
 
     public static void main(String[] args) {
-        int arr[][];                        //объявление массива
-        arr = new int[ROWS][COLUMNS];      //формирование массива
+        int arr [] [] = new int[ROWS][COLUMNS];      //формирование массива
         for (int i = 0; i < ROWS; i++) {
-            int counter = 4 - i;           // счётчик, в 1 строке 5 нулей, во 2 строке 4 нуля и т.д.
+            int counter = ROWS - i;           // счётчик, в 1 строке 5 нулей, во 2 строке 4 нуля и т.д.
             for (int j = 0; j < COLUMNS; j++) {
-                if (j <= counter || j >= 10 - counter) {
+                if (j < counter || j > 10 - counter) {
                     System.out.print(' ');
-                } else
+                } else {
                     System.out.print('*');
-
+                }
             }
             System.out.print('\n');
         }
