81730650b1fa3acc4ff061f5d2da78524764f246
Wesfall <krixaliks@gmail.com>
2018-10-02 23:55:06 +0300

    8th commit with changes in array filling

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
-    private final static int ROWS = 6, COLUMNS = 11;        //константы - 6 строк, 11 столбцов
+    private final static int ROWS = 5, COLUMNS = 11;        //константы - 6 строк, 11 столбцов
 
     public static void main(String[] args) {
         for (int i = 0; i < ROWS; i++) {
             int countStarsInRow = ROWS - i;
             for (int j = 0; j < COLUMNS; j++) {
-                if (j < countStarsInRow || j > 10 - countStarsInRow) {
+                if (j < countStarsInRow || j >= COLUMNS - countStarsInRow) {
                     System.out.print(' ');
                 } else {
                     System.out.print('*');
